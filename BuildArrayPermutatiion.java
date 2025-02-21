baa42696eb2f13a511f57d11a5ccb11320371402
Abdul Hamdan <155870428+ahamdan3602@users.noreply.github.com>
2025-02-20 23:07:13 -0500

    Update BuildArrayPermutatiion.java
    some commented example of how it works

--- BuildArrayPermutatiion.java
+++ BuildArrayPermutatiion.java
@@ -3,6 +3,15 @@ class BuildArrayPermutation {
         int[] sol = new int[nums.length];
         for (int i = 0; i < nums.length;i++) {
             sol[i] = nums[nums[i]];
+           /*
+            i = 0, nums[i] = 0,
+            i = 1, nums[i] = 2.
+            i = 2, nums[i] = 1 
+
+            nums[0] = 0
+            nums[2] = 1
+            nums[1] = 2
+            */
          
         }
         return sol;
