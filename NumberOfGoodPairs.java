cbb35da7b4106aab6231e6da77c26bf660a8b6fc
Abdul Hamdan <155870428+ahamdan3602@users.noreply.github.com>
2025-02-20 23:16:35 -0500

    Create NumberOfGoodPairs.java

---
+++ NumberOfGoodPairs.java
@@ -0,0 +1,15 @@
+class NumberOfGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int gp = 0;
+        for (int i =0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length;j++) {
+                if (nums[i] == nums[j] && i < j) {
+                    gp++;
+                }
+            }
+        }
+
+        return gp;
+        
+    }
+}
